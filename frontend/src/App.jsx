a523ce601317485c609c9784c7d8f5fd09217be7
Harkirat Singh <harkirat96@gmail.com>
2023-12-31 20:06:56 +0530

    added frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      hi there
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,10 @@
+
+export function CreateTodo() {
+    return <div>
+        <input type="text" placeholder="title"></input> <br />
+        <input type="text" placeholder="description"></input> <br />
+
+        <button>Add a todo</button>
+    </div>
+}
+
